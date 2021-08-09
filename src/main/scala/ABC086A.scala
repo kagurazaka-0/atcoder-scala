5e0143dd0b08f59b32e30effc5c01c2033a5cce3
katsuya_U <katsuya_u@icloud.com>
2021-08-09 15:28:04 +0900

    feat: ABC086Aを回答

---
+++ src/main/scala/ABC086A.scala
@@ -0,0 +1,15 @@
+import java.util.Scanner
+object ABC086A {
+  def run(line: String): String = {
+    val multiply = line.split(" ").map(_.toInt).reduce(_ * _)
+    if (multiply % 2 == 0) {
+      "Even"
+    } else {
+      "Odd"
+    }
+  }
+
+  def main(args: Array[String]) = {
+    println(run(new Scanner(System.in).nextLine()))
+  }
+}
