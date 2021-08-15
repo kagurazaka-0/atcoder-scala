b8da83d02549d030f1ec650d985370761dc1f78e
katsuya_U <katsuya_u@icloud.com>
2021-08-16 04:19:08 +0900

    feat: ABC085Cの実行速度を改善

--- src/main/scala/ABC085C.scala
+++ src/main/scala/ABC085C.scala
@@ -7,29 +7,21 @@ object ABC085C {
     // NOTE: viewをすることで、遅延評価にする
     // https://tanishiking24.hatenablog.com/entry/scala-generator
     // https://r9.hateblo.jp/entry/2016/01/12/234144
-    val rangeToCount = (0 to count).view
+    // val rangeToCount = (0 to count).view
     // val rangeToCount = (count to 0 by -1).view
 
     // NOTE: for内包表記は下からloopを開始する
     val seq = for {
-      z <- rangeToCount
-      y <- rangeToCount
-      x <- rangeToCount
-      if (x + y + z == count)
-
-      (a, b, c) = (x * 10000, y * 5000, z * 1000)
-      // if (a <= requestYen && b <= requestYen && c <= requestYen)
-    } yield ((x, y, z), a + b + c)
-    // sum = x * 10000 + y * 5000 + z * 1000
-    // if (sum <= requestYen)
-    // } yield ((x, y, z), sum)
+      _10k <- (0 to count).view
+      _5k <- (0 to count - _10k).view
+      _1k = count - _10k - _5k
+      sum = _10k * 10000 + _5k * 5000 + _1k * 1000
+    } yield ((_10k, _5k, _1k), sum)
 
     val result = seq
       .find { case (_, yen) => yen == requestYen }
       .map(_._1)
       .getOrElse((-1, -1, -1))
-
-    // val result = seq.to()
 
     s"${result._1} ${result._2} ${result._3}"
   }
